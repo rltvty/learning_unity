5942690b0d33a13a54bfbd8be5482822ce889856
Eric Pinzur <ericpinzur@otherinbox.com>
2019-05-16 23:14:01 -0500

    fractal progress

---
+++ Assets/Fractal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+
+    private int depth;
+
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").AddComponent<Fractal>().
+                Initialize(this, Vector3.up);
+            new GameObject("Fractal Child").AddComponent<Fractal>().
+                Initialize(this, Vector3.right);
+        }
+    }
+
+    public float childScale;
+
+    private void Initialize(Fractal parent, Vector3 direction)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
